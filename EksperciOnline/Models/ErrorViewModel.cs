f9e8d444577c81b4be691cb5d3cef67aea3d7926
Rogoziński <dorogozi01@gmail.com>
2024-06-18 18:06:43 +0200

    Base

---
+++ EksperciOnline/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EksperciOnline.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
