f4c7fc7a6180d7b66dc78ad295c14c10c895475d
Rogoziński <dorogozi01@gmail.com>
2024-06-27 19:12:34 +0200

    DevBase

---
+++ EksperciOnline/Controllers/AdminKategorieController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EksperciOnline.Controllers
+{
+    public class AdminKategorieController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
