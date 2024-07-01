8ea33e2b4369f5a52771b10fbfda40cca5aff494
MarnidG <kubix1xpl2@gmail.com>
2024-07-01 12:20:33 +0200

    2
    2

---
+++ EksperciOnline/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EksperciOnline.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- EksperciOnline/Program.cs
+++ EksperciOnline/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the containeradsasdasf.
+// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<EksperciOnlineDbContext>(options =>
